c23b4201317835602978b6a51ad5075702721680
Jim Halkyard <jim.halkyard@sonarsource.com>
2026-04-16 11:57:47 +0800

    Fixing issues raised

--- src/main/java/demo/security/servlet/FileServlet.java
+++ src/main/java/demo/security/servlet/FileServlet.java
@@ -9,10 +9,9 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-
 @WebServlet("/files")
-public class FileServlet extends HttpServlet 
-{
+public class FileServlet extends HttpServlet {
+    private static final long serialVersionUID = 1;
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String data = request.getParameter("data");
