260a76d5220981249d592ce52b1928036ccbb820
Jim Halkyard <jim.halkyard@sonarsource.com>
2026-04-16 11:23:36 +0800

    Comment out non-used import

--- src/main/java/demo/security/servlet/Insecure.java
+++ src/main/java/demo/security/servlet/Insecure.java
@@ -4,7 +4,7 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.security.KeyPair;
+//import java.security.KeyPair;
 import java.security.KeyPairGenerator;
 import java.security.NoSuchAlgorithmException;
 import java.sql.Connection;
